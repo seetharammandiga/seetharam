90f16e6e5e27829c2a3da6056d5e01d3845e05ef
seetharammandiga <seetharam.mandiga27@gmail.com>
2017-08-25 23:38:30 +0530

    commit and push

--- javastandalone/src/com/nani/javastandalone/MainClass.java
+++ javastandalone/src/com/nani/javastandalone/MainClass.java
@@ -5,8 +5,6 @@ import java.util.HashMap;
 public class MainClass {
 
 	public static void main(String[] args) {
-		
-
 		/*
 		 * Student student = new Student(); student.setAge("20");
 		 * student.setSname("seetharam"); student.setStudent(student); //
